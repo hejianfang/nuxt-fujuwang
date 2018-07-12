94a6b72fd70cdce6abaf9144055176ec5425ccde
hejianfang <1343574826@qq.com>
2018-07-12 16:12:59 +0800

    完善

--- pages/test.vue
+++ pages/test.vue
@@ -1,23 +1,15 @@
 <template>
    <div>
 
-       <mapTab></mapTab>
+
    </div>
 </template>
 
 <script>
-
-     import rightBox from '~/components/rightbox'
-     import header2 from '~/components/header2'
-     import myBox from '~/components/my-box'
-     import fangdajing from '~/components/fangdajing'
-     import swicthpic from '~/components/swicthpic'
      import mapTab from '~/components/mapTab'
     export default {
         name: "banner",
          components: {
-             fangdajing,
-             swicthpic,
              mapTab
          },
 
